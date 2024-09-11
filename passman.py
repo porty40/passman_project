68d7e9d8035244b0d1ffbf9bd93ef9d108545007
Oleksandr Porteritto <139705491+porty40@users.noreply.github.com>
2024-09-11 14:35:45 +0300

    +session ~mp-reset

--- passman.py
+++ passman.py
@@ -10,29 +10,48 @@ from argon2.exceptions import VerifyMismatchError
 
 ph = PasswordHasher(time_cost=1, memory_cost=512, parallelism=4)
 
+session = {"logged_in": False, "username": None}
+
 @click.group()
 def cli() -> None:
     """A simple command line password manager."""
 
 #password slot operations section
 @cli.command()
-@click.option('--password', prompt=True, hide_input=True)
-@click.option('--list-all', help='List all password slots')
-def list_slots(password: str) -> None:
+@click.option('--slot-name', prompt='Enter the slot name: ', help='Create specified slot')
+@click.option('--slot-content', prompt='Enter the content of the slot: ', help='Specify the content of the slot', hide_input=True)
+@click.option('--password', prompt='Enter the master password: ', hide_input=True)
+def add_slot(slot_name: str, slot_content: str, password: str) -> None:
+    """Creates a slot in the vault."""
     pass
 
 @cli.command()
-@click.option('--password', prompt=True, hide_input=True)
-@click.option('--slot', prompt='Enter the password slot name: ', help='Access specified password slot')
+@click.option('--slot-name', prompt='Enter the slot name: ', help='Create specified slot')
+@click.option('--password', prompt='Enter the master password: ', hide_input=True, confirmation_prompt=True)
+def delete_slot(slot_name: str, password: str) -> None:
+    """Creates a slot in the vault."""
+    pass
+
+@cli.command()
+@click.option('--slot-name', prompt='Enter the slot name: ', help='Access specified slot')
+@click.option('--password', prompt='Enter the master password: ', hide_input=True)
 @click.option('--clip', help='Copy the revealed slot to the clipboard')
 def reveal_slot(slot: str, password: str) -> None:
+    """Reveals specified slot or copies it to the clipboard."""
+    pass
+
+@cli.command()
+@click.option('--password', prompt='Enter the master password: ', hide_input=True)
+def list_slots(password: str) -> None:
+    """Lists all the slots of the vault."""
     pass
 
 #user/ master password operations section
 @cli.command()
-@click.argument('username')
-@click.option('--password', prompt='Enter the password: ', hide_input=True, confirmation_prompt=True)
+@click.argument('username') #SHOULD LOG OUT TO CREATE A USER?
+@click.option('--password', prompt='Enter the master password: ', hide_input=True, confirmation_prompt=True)
 def user_set(username: str, password: str) -> None:
+    """Sets up a user account (username: master password)."""
     accounts = 'acc.json'
     try:
         if os.path.exists(accounts):
@@ -54,21 +73,25 @@ def user_set(username: str, password: str) -> None:
         click.echo(f"Error: {e}")
 
 @cli.command()
-@click.argument('username')
-@click.option('--old-password', prompt='Enter the old password: ', hide_input=True)
-@click.option('--new-password', prompt='Enter the new password: ', hide_input=True, confirmation_prompt=True)
-def mp_reset(username: str, old_password: str, new_password: str) -> None:
+@click.option('--old-password', prompt='Enter the old master password: ', hide_input=True)
+@click.option('--new-password', prompt='Enter the new master password: ', hide_input=True, confirmation_prompt=True)
+def mp_reset(old_password: str, new_password: str) -> None:
+    """Resets the master password for the current user."""
     accounts = 'acc.json'
+    if not session["logged_in"]:
+        click.echo("You must log in first.")
+        return
+    username = session["username"]
     try:
         if not os.path.exists(accounts):
-            click.echo("No users have been created yet.")
+            click.echo(f"'{accounts}' file is missing")
             return
 
         with open(accounts, 'r') as file:
             users = json.load(file)
 
         if username not in users:
-            click.echo(f"User '{username}' does not exist.")
+            click.echo(f"User '{username}' does not exist. Potential integrity loss: {accounts}")
             return
 
         if ph.verify(users[username], old_password):
@@ -85,11 +108,57 @@ def mp_reset(username: str, old_password: str, new_password: str) -> None:
         click.echo(f"Error: {e}")
 
 #assistive functions
-def master_pass_valid(m_password: str) -> bool:
-    pass
-
 def input_valid(input: str) -> bool:
     pass
 
+def logout() -> None:
+    """Logout"""
+    if not session["logged_in"]:
+        click.echo("No user is currently logged in.")
+        return
+
+    session["logged_in"] = False
+    click.echo(f"User '{session["username"]}' logged out successfully.")
+    session["username"] = None
+
+def session():
+    """Starts the session."""
+    while True:
+        cmd = input("('exit' to quit)~# ")
+        if cmd == "exit":
+            if session["logged_in"]:
+                logout()
+            break
+        else:
+            try:
+                cli.main(args=cmd.split(), prog_name="passman", standalone_mode=False)
+            except SystemExit:
+                pass
+
+@cli.command()
+@click.argument('username')
+@click.option('--password', prompt='Enter your password: ', hide_input=True)
+def login(username: str, password: str) -> None:
+    """Login"""
+    accounts = 'acc.json'
+    try:
+        if not os.path.exists(accounts):
+            click.echo("No users available. Please create a user first.")
+            return
+
+        with open(accounts, 'r') as file:
+            users = json.load(file)
+
+        if username not in users:
+            click.echo(f"User '{username}' does not exist.")
+            return
+
+        ph.verify(users[username], password)
+        session["logged_in"] = True
+        session["username"] = username
+        click.echo(f"User '{username}' logged in successfully.")
+    except (IOError, json.JSONDecodeError, VerifyMismatchError) as e:
+        click.echo(f"Error: {e}")
+
 if __name__ == '__main__':
-    cli()
+    session()
