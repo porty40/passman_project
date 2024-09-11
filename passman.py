23400743d55b462636d549d5293aba2a917b5961
Oleksandr Porteritto <139705491+porty40@users.noreply.github.com>
2024-09-11 21:47:05 +0300

    +CryptoTest ~minor-ch

--- passman.py
+++ passman.py
@@ -8,11 +8,23 @@ import re
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
 #import pyperclip
-#from Crypto.Cipher import AES
+from Crypto.Cipher import AES
 
 ph = PasswordHasher(time_cost=1, memory_cost=512, parallelism=4)
 
 session = {"logged_in": False, "username": ""}
+
+users_dir = "./users"
+accounts = 'acc.json'
+user_path = f'{users_dir}/{session["username"]}'
+
+salt_len = 16
+key_len = 32
+blk_size = AES.block_size
+
+inv_usr_name = "Invalid username format:\n\tUsername should contain from 2 to 10\n\talphanumerical characters without spaces."
+inv_pass = "Invalid password format:\n\tPassword should contain a minimum of 8 characters,\n\tincluding at least one uppercase letter,\n\tone lowercase letter, one digit,\n\tand one special character (@#$!%*?&)."
+inv_slot_name = "Invalid slot name format:\n\tSlot name should contain from 2 to 10\n\talphanumerical characters without spaces."
 
 def is_valid_name(username: str) -> bool:
     rx = r'^([a-zA-Z\d]{2,10})$'
@@ -30,6 +42,9 @@ def sanitize(input: str) -> str:
     return sanitized
 '''
 
+def derive_key(password: str, salt: bytes) -> bytes:
+    pass
+
 @click.group()
 def cli() -> None:
     """A simple command line password manager."""
@@ -42,14 +57,45 @@ def cli() -> None:
 @click.option('--password', prompt='Enter the master password: ', hide_input=True)
 def add_slot(slot_name: str, slot_content: str, password: str) -> None:
     """Creates a slot in the vault."""
-    pass
+    if not session["logged_in"]:
+        click.echo("No user is currently logged in.")
+        return
+    if not is_valid_name(slot_name):
+        click.echo(inv_slot_name)
+        return
+    try:
+        with open(accounts, 'r') as file:
+            users = json.load(file)
+        ph.verify(users[session["username"]], password)
+
+        if not os.path.exists(user_path):
+            os.makedirs(user_path)
+
+        vault_path = f'{user_path}/vault.json'
+        slot_path = f'{user_path}/{slot_name}.bin'
+        slots = {}
+
+        slot_salt = os.urandom(salt_len)
+        enc_key = derive_key(password, slot_salt)
+
+        ### FUNC ++
+
+        slots[slot_name] = slot_salt
+        with open(user_path, 'w') as file:
+            json.dump(slots, vault_path, indent=4)
+
+    except (IOError, json.JSONDecodeError, VerifyMismatchError) as e:
+        click.echo(f"Error: {e}")
 
 @cli.command()
 @click.option('--slot-name', prompt='Enter the slot name: ', help='Create specified slot')
 @click.option('--password', prompt='Enter the master password: ', hide_input=True, confirmation_prompt=True)
 def del_slot(slot_name: str, password: str) -> None:
     """Creates a slot in the vault."""
-    pass
+    if not session["logged_in"]:
+        click.echo("No user is currently logged in.")
+        return
+    username = session["username"]
 
 @cli.command()
 @click.option('--slot-name', prompt='Enter the slot name: ', help='Access specified slot')
@@ -57,13 +103,19 @@ def del_slot(slot_name: str, password: str) -> None:
 @click.option('--clip', help='Copy the revealed slot to the clipboard')
 def show_slot(slot: str, password: str) -> None:
     """Reveals specified slot or copies it to the clipboard."""
-    pass
+    if not session["logged_in"]:
+        click.echo("No user is currently logged in.")
+        return
+    username = session["username"]
 
 @cli.command()
 @click.option('--password', prompt='Enter the master password: ', hide_input=True)
 def list_slots(password: str) -> None:
     """Lists all the slots of the vault."""
-    pass
+    if not session["logged_in"]:
+        click.echo("No user is currently logged in.")
+        return
+    username = session["username"]
 
 #user/ master password operations section
 @cli.command()
@@ -75,12 +127,11 @@ def user_set(username: str, password: str) -> None:
         click.echo("Log out to create new user account.")
         return
     if not is_valid_name(username):
-        click.echo("Invalid username format:\n\tUsername should contain from 2 to 10\n\talphanumerical characters without spaces.")
+        click.echo(inv_usr_name)
         return
     if not is_valid_password(password):
-        click.echo("Invalid password format:\n\tPassword should contain a minimum of 8 characters,\n\tincluding at least one uppercase letter,\n\tone lowercase letter, one digit,\n\tand one special character (@#$!%*?&).")
+        click.echo(inv_pass)
         return
-    accounts = 'acc.json'
     try:
         if os.path.exists(accounts):
             with open(accounts, 'r') as file:
@@ -95,6 +146,9 @@ def user_set(username: str, password: str) -> None:
 
         with open(accounts, 'w') as file:
             json.dump(users, file, indent=4)
+
+        user_path = f'{users_dir}/{username}'
+        os.makedirs(user_path)
 
         click.echo(f"User '{username}' has been added successfully.")
     except (IOError, json.JSONDecodeError) as e:
@@ -141,7 +195,6 @@ def pass_reset(old_password: str, new_password: str) -> None:
 @click.option('--password', prompt='Enter your password: ', hide_input=True)
 def login(username: str, password: str) -> None:
     """Login"""
-    accounts = 'acc.json'
     try:
         if not os.path.exists(accounts):
             click.echo("No users available. Please create a user first.")
